275aec24b056d30f86c6ca0b4282481ac6fcdbcd
fh-aihara <90545280+fh-aihara@users.noreply.github.com>
2024-05-22 22:56:55 +0900

    add method

--- src/views/Query.vue
+++ src/views/Query.vue
@@ -57,6 +57,7 @@ export default {
   computed: {},
   methods: {
     executeQuery() {
+      console.log("executing query...");
       api
         .postBigquery({ sql: this.sqlQuery })
         .then((response) => {
